f0150bbda936cede2a30e4c5dd4a0300e36861a2
MohanRaj <mraj86698@gmail.com>
2022-06-24 16:54:47 +0530

    Added start from position 0

--- src/snakeLadder/SnakesLadder.java
+++ src/snakeLadder/SnakesLadder.java
@@ -3,6 +3,8 @@ package snakeLadder;
 public class SnakesLadder {
 	public static void main(String args[]) {
 		System.out.println("Welcome to snake and ladder Game");
+		int position = 0;
+        System.out.println("Position of player = " + position);
 	}
 
 }
