139baa54f7d9b3662ea708e30e4d2fdd51e648c3
MohanRaj <mraj86698@gmail.com>
2022-06-24 17:06:49 +0530

    Added Rolling the Dice

--- src/snakeLadder/SnakesLadder.java
+++ src/snakeLadder/SnakesLadder.java
@@ -3,8 +3,8 @@ package snakeLadder;
 public class SnakesLadder {
 	public static void main(String args[]) {
 		System.out.println("Welcome to snake and ladder Game");
-		int position = 0;
-        System.out.println("Position of player = " + position);
+		int dice = (int)(Math.floor((Math.random()* 6)+ 1));
+		System.out.println("The Dice Rolls are "+""+dice);
 	}
 
 }
